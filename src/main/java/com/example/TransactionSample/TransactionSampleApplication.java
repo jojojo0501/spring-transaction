4e24ec47e9f0ab2eda2acf5fc4cb81301408a44e
jojojo0501 <sarukiti0418@outlook.jp>
2022-09-02 23:49:39 +0900

    first commit

---
+++ src/main/java/com/example/TransactionSample/TransactionSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.TransactionSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TransactionSample/TransactionSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TransactionSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
